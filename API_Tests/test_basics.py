de3af3e4d6469a3cfc40aa2236054c29872a0b5d
Carlos Paiva <paivakk22@gmail.com>
2023-08-24 18:55:39 -0300

    Added test for POST

--- API_Tests/test_basics.py
+++ API_Tests/test_basics.py
@@ -17,14 +17,26 @@ def test_get_pet_check_response():
     assert len(data) > 0, "empty response"
 
 
-@pytest.mark.currentTest
 def test_get_pet_by_id():
     url = base_url + pet_id
     header = {'Content-Type': 'application/json'}
-    print("Request URL: ", url)
     response = requests.get(url, verify=False, headers=header)
     data = response.json()
     print(json.dumps(data, indent=3))
 
     assert data['id'] == 8989
     assert data['name'] == "doggie"
+
+
+@pytest.mark.currentTest
+def test_get_post_new_pet():
+    url = base_url
+    header = {'Content-Type': 'application/json'}
+    payload = {"id": 2801, "name": "swan", "status": "available"}
+    response = requests.post(url, verify=True, json=payload, headers=header)
+    data = response.json()
+    print(json.dumps(data, indent=3))
+
+    assert data['id'] == 2801
+    assert data['name'] == 'swan'
+    assert data['status'] == 'available'
