fff72d047464b80ddbfec004ca99324b858677cc
Carlos Paiva <paivakk22@gmail.com>
2023-08-26 08:36:41 -0300

    Added put request

--- API_Tests/test_pets.py
+++ API_Tests/test_pets.py
@@ -1,5 +1,5 @@
 import pytest
-from utils.utils import get_api_return_response_data
+from utils.utils import get_api_return_response_data, put_api_update_data
 from utils.utils import time_limit_response_api
 from utils.env_configparser import get_pet_env_qa
 
@@ -7,11 +7,23 @@ base_url = get_pet_env_qa()
 pet_id = "8989"
 
 
-@pytest.mark.currentTest
 def test_get_by_id():
     url = base_url + pet_id
     data, resp_status, time_taken = get_api_return_response_data(url)
     performance = time_limit_response_api(time_taken)
     assert data['id'] == int(pet_id)
     assert resp_status == 200
+    assert performance is True
+
+
+@pytest.mark.currentTest
+def test_update_by_id():
+    url = base_url
+    payload = {"id": int(pet_id), "name": "test automation", "status": "pending"}
+    data, resp_status, time_taken = put_api_update_data(url, payload)
+    performance = time_limit_response_api(time_taken)
+    print("Update data: ", data)
+    assert data['id'] == int(pet_id)
+    assert data['name'] == 'test automation'
+    assert data['status'] == 'pending'
     assert performance is True
