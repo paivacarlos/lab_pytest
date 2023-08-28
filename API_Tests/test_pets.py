e14c0df6b4fafd00840c0190211ba884a1d2c393
Carlos Paiva <paivakk22@gmail.com>
2023-08-28 14:25:29 -0300

    Added function request delete

--- API_Tests/test_pets.py
+++ API_Tests/test_pets.py
@@ -1,5 +1,5 @@
 import pytest
-from utils.utils import get_api_return_response_data, put_api_update_data
+from utils.utils import get_api_return_response_data, put_api_update_data,delete_api_data
 from utils.utils import time_limit_response_api
 from utils.env_configparser import get_pet_env_qa
 
@@ -16,7 +16,6 @@ def test_get_by_id():
     assert performance is True
 
 
-@pytest.mark.currentTest
 def test_update_by_id():
     url = base_url
     payload = {"id": int(pet_id), "name": "test automation", "status": "pending"}
@@ -26,4 +25,16 @@ def test_update_by_id():
     assert data['id'] == int(pet_id)
     assert data['name'] == 'test automation'
     assert data['status'] == 'pending'
+    assert performance is True
+
+
+@pytest.mark.currentTest
+def test_delete_by_id():
+    url = base_url + pet_id
+    api_key = {'api_key': 'apikeys123'}
+    data, resp_status, time_taken = delete_api_data(url, api_key)
+    performance = time_limit_response_api(time_taken)
+    print("Delete data: ", data)
+    assert data['message'] == pet_id
+    assert data['code'] == 200
     assert performance is True
